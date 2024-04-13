e99fc665d369afe40ffa1370d5ac6860bfd8a99c
Rumman <chowdhuryrumman78@gmail.com>
2024-04-13 23:21:58 +0530

    added

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,48 +2,46 @@ import BedroomImage from "../assets/images/bedroom.png";
 
 function HomePage() {
   return (
-    <div className="container relative min-h-screen bg-background ">
-      <div className="text-3xl font-normal text-brandPrimary mb-4 p-3">
-        RentKar
-      </div>
-      <div className="absolute top-0 right-4">
-        <button className="text-brandPrimary font-normal py-2 px-4 rounded cursor-pointer">
+    <div className=" bg-background ">
+      <div className="flex ">
+        <div className="text-3xl font-normal m-5 w-2/4">RentKar</div>
+
+        <button className="  font-normal flex  justify-end items-center m-5  rounded cursor-pointer w-2/4 ">
           Log in
         </button>
       </div>
 
-      <div className="w-5/6 max-w-md overflow-hidden">
+      <div className="w-4/5 max-w-md ">
         <img
           src={BedroomImage}
           alt="Bedroom"
-          className="w-full h-auto rounded-l-lg rounded-r-full"
+          className=" h-3/6 rounded-l-lg rounded-r-full"
         />
       </div>
 
       <div className="my-8">
-        <h2 className="text-3xl font-regular text-brandPrimary mb-4 px-20">
+        <h2 className="text-3xl font-regular mx-9 my-4 flex items-center justify-start ">
           Why Us?
         </h2>
-        <div
-          className="bg-card rounded-3xl shadow-lg p-4 mx-auto"
-          style={{ width: "90vw" }}
-        >
-          <p className="text-gray-700">
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using 'Content here, content
-            here'.
-          </p>
+        <div className="flex flex-col justify-center items-center ">
+          <div className="bg-card rounded-3xl shadow-lg w-10/12 p-4 ">
+            <p className="text-gray-700">
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsum is that it has a more-or-less normal
+              distribution of letters, as opposed to using 'Content here,
+              content here'.
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <button className="bg-BT text-black font-regular py-2 px-4 rounded-full w-38 mb-4">
-          Sign in as a room owner
-        </button>
-        <button className="bg-BT text-black font-regular py-2 px-4 rounded-full w-38 mb-4">
-          Sign in as a tenant
-        </button>
+        <div className="flex flex-col items-center ">
+          <button className="bg-BT text-black font-regular m-2 h-10 rounded-full w-2/3  flex items-center justify-center">
+            Sign in as a renter
+          </button>
+          <button className="bg-BT text-black font-regular m-2 h-10 rounded-full w-2/3 flex items-center justify-center">
+            Sign in as a tenant
+          </button>
+        </div>
       </div>
       <div class="text-center mt-4 bg-card py-2">
         <p class="text-black-500">footer %c copyright</p>
