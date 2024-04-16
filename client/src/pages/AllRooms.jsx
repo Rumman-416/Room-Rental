2b6183100af6ac8b910ace5940ef96ac61e03833
Rumman <chowdhuryrumman78@gmail.com>
2024-04-16 23:24:31 +0530

    style allrooms added

--- client/src/pages/AllRooms.jsx
+++ client/src/pages/AllRooms.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-
+import { FaSearch } from "react-icons/fa";
 const AllRooms = () => {
   const [rooms, setRooms] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
@@ -30,45 +30,51 @@ const AllRooms = () => {
   );
 
   return (
-    <div className=" m-5">
+    <div className="m-3">
       <div>
         <form>
-          <input
-            type="text"
-            className="h-8 rounded-md"
-            placeholder="search city or state..."
-            onChange={handleSearchInputChange}
-          />
+          <div className="border border-black flex h-8 rounded-md items-center">
+            <input
+              type="text"
+              className="h-full w-full"
+              placeholder="search city or state..."
+              onChange={handleSearchInputChange}
+            />
+            <FaSearch className="text-2xl text-gray-300" />
+          </div>
         </form>
       </div>
       <h5>All Rooms</h5>
 
       <div>
         {filteredRooms.map((room) => (
-          <div key={room._id} className=" border-2 m-2 p-5 border-black">
+          <div key={room._id} className="  p-3.5 shadow-2xl rounded-3xl m-1">
             <Link to={`/room/${room._id}`}>
-              <h3>{room.name}</h3>
-              <p>
-                Phone: <span>{room.phone}</span>
-              </p>
-              <p>Address: {room.address}</p>
-              <p>City: {room.city}</p>
-              <p>State: {room.state}</p>
-              <p>Rent: {room.rent}</p>
-              <p>Number of Guests: {room.numOfGuest}</p>
               {/* Render images if available */}
               {room.images && (
-                <div>
+                <div className=" bg-slate-800 rounded-2xl">
                   {room.images.map((image, index) => (
                     <img
                       key={index}
                       src={`http://localhost:3000/${image}`}
                       alt={`Room ${index + 1}`}
-                      className="h-20 w-36 m-2"
+                      className="h-40 w-full rounded-2xl"
                     />
                   ))}
                 </div>
               )}
+              <div className=" pl-2 pr-2">
+                <div className="flex justify-between">
+                  <h3 className="font-bold text-xl">{room.name}</h3>
+                  <p>rating</p>
+                </div>
+                <p>City: {room.city}</p>
+                <p>State: {room.state}</p>
+                <div className="flex flex-row">
+                  <p>Rent: ₹ </p> <p className="font-bold">{room.rent}</p>
+                </div>
+                <p>Number of Guests: {room.numOfGuest}</p>
+              </div>
             </Link>
           </div>
         ))}
